acf47e9f8738143849227018c0b88877c1a28812
stawberryslayer <ywang2776@wisc.edu>
2024-01-04 01:04:31 -0600

    successfully add a custom block to workspace

--- my-ts/src/App.tsx
+++ my-ts/src/App.tsx
@@ -1,11 +1,42 @@
 import { BlocklyWorkspace } from 'react-blockly';
+import Blockly, { Block } from 'blockly'
 import './App.css';
 
 const INITIAL_XML = `<xml xmlns="http://www.w3.org/1999/xhtml">
   <block type="text" x="70" y="30">
     <field name="TEXT">Hello World</field>
   </block>
+  <block type="set_color" x="300" y="300">
+    <field name="H"></field>
+    <field name="S"></field>
+    <field name="B"></field>
+  </block> 
 </xml>`;
+
+Blockly.Blocks['set_color'] = {
+  init: function(){
+    let block = this as Block;
+    block.appendDummyInput()
+      .appendField("set color to");
+    block.appendValueInput("H")
+      .setCheck(null)
+      .appendField("H");
+    block.appendValueInput("S")
+      .setCheck(null)
+      .appendField("S");
+    block.appendValueInput("B")
+      .setCheck(null)
+      .appendField("B");
+    block.setInputsInline(true);
+    block.setPreviousStatement(true, null);
+    block.setNextStatement(true, null);
+    block.setColour(230);
+    block.setTooltip("");
+    block.setHelpUrl("");
+  }
+};
+
+
 
 function App() {
   return (
