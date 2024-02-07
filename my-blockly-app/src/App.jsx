632a94815ddc43f3d7dfd07a2fa4578f4b105633
stawberryslayer <ywang2776@wisc.edu>
2024-02-07 14:47:06 -0600

    move Blockly into its own component

--- my-blockly-app/src/App.jsx
+++ my-blockly-app/src/App.jsx
@@ -7,49 +7,17 @@ import './App.css'; // Assuming index.css is renamed to App.css
 import { blocks } from './blocks/text';
 import { forBlock } from './generators/javascript';
 import { javascriptGenerator } from 'blockly/javascript';
-import {save, load} from './serialization';
-import {toolbox} from './toolbox';
 import './index.css';
 import TopBar from './Topbar';
+import BlocklyInterface from './Blockly-interface';
 // Register the blocks and generator with Blockly
 Blockly.common.defineBlocks(blocks);
 Object.assign(javascriptGenerator.forBlock, forBlock);
 function App() {
-  useEffect(() =>{
-    if (!document.querySelector('.blocklySvg')) { 
-  const ws = Blockly.inject('blocklyDiv', { toolbox:toolbox});
-
-  javascriptGenerator.addReservedWords('code');
-  const runCode = () => {
-    console.log("runcode clicked")
-    const code = javascriptGenerator.workspaceToCode(ws);
-    eval(code);
-  }
-  //add click event listener to run button
-  document.getElementById('runButton').addEventListener('click', runCode);
-  // Every time the workspace changes state, save the changes to storage.
-  ws.addChangeListener((e) => {
-    // UI events are things like scrolling, zooming, etc.
-    // No need to save after one of these.
-    if (e.isUiEvent) return;
-    save(ws);
-})}},[])
-
-
-
   return (
-    
     <div>
       <TopBar/>
-    <div id="pageContainer">
-      <div id="blocklyDiv" style={{ height: '480px', width: '800px' }}></div>
-      <xml id="toolbox" style={{ display: 'none' }}>
-        {/* Toolbox XML goes here */}
-      </xml>
-      {/* <div id="outputPane">
-        <pre id="generatedCode"><code></code></pre>
-      </div> */}
-    </div>
+    <BlocklyInterface/>
     <Scene/>
     </div>
   );
