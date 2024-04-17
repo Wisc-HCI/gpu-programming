1513b755be1acedbda28b36d8eabad66ebfc990a
stawberryslayer <ywang2776@wisc.edu>
2024-04-16 20:17:38 -0500

    Activity tracker

---
+++ src/components/ActivityTracker.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function ActivityTracker(props) {
+    //Record user activity
+    //Button clicks
+    //Updates to code
+    //Progress in curriculum
+
+    return{}
+}
