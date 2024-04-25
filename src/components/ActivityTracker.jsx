a84f958e4d13c874328dcf52e25c001ac9360fb0
stawberryslayer <ywang2776@wisc.edu>
2024-04-24 22:34:04 -0500

    Styling with eslint

--- src/components/ActivityTracker.jsx
+++ src/components/ActivityTracker.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-
-export let activityLog = '';
+export let activityLog = "";
 export const appendActivity = (text) => {
-    activityLog = activityLog.concat("\n", text);
+  activityLog = activityLog.concat("\n", text);
 };
 
 export default function ActivityTracker(props) {
-    //Record user activity
-    //Updates to code
-    //Progress in curriculum
-    return{}
+  //Record user activity
+  //Updates to code
+  //Progress in curriculum
+  return {};
 }
--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -1,47 +1,52 @@
 import useStore from "../Store";
-import { MeshLookupTable } from '../Misty-Robot/MeshLookup.js';
-import {Scene} from 'robot-scene';
-import IconButton from '@mui/material/IconButton';
-import RestartAltIcon from '@mui/icons-material/RestartAlt';
-import { useShallow } from 'zustand/react/shallow';
+import { MeshLookupTable } from "../Misty-Robot/MeshLookup.js";
+import { Scene } from "robot-scene";
+import IconButton from "@mui/material/IconButton";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import { useShallow } from "zustand/react/shallow";
 
-import React from 'react';
+import React from "react";
 import { Box } from "@mui/material";
 
 export default function Simulator(props) {
-    const resetSim = useStore(useShallow((state) => state.resetSim));
+  const resetSim = useStore(useShallow((state) => state.resetSim));
 
-    const otherArgs = {
-        displayTfs: false,
-        displayGrid: true,
-        isPolar: false,
-        backgroundColor: "#3a3a3a",
-        planeColor: "#3a3a3a",
-        highlightColor: "#ffffff",
-        plane: 0,
-        fov: 60,
-        ar: false,
-        vr: false,
-    }
+  const otherArgs = {
+    displayTfs: false,
+    displayGrid: true,
+    isPolar: false,
+    backgroundColor: "#3a3a3a",
+    planeColor: "#3a3a3a",
+    highlightColor: "#ffffff",
+    plane: 0,
+    fov: 60,
+    ar: false,
+    vr: false,
+  };
 
-    return (
-        <Box height={"100%"}>
-            <Scene style={{ "overflow": "hidden"}} store={useStore} meshLookup={MeshLookupTable} {...otherArgs}/>
-            <IconButton 
-                variant="contained" 
-                aria-label="restart" 
-                style={{
-                    position: "absolute", 
-                    right: "0px", 
-                    top: "0px", 
-                    backgroundColor: "white", 
-                    marginRight: "5px", 
-                    marginTop: "5px"
-                }}
-                onClick={resetSim}>
-                <RestartAltIcon />
-            </IconButton>
-        </Box>
-    )
-
+  return (
+    <Box height={"100%"}>
+      <Scene
+        style={{ overflow: "hidden" }}
+        store={useStore}
+        meshLookup={MeshLookupTable}
+        {...otherArgs}
+      />
+      <IconButton
+        variant="contained"
+        aria-label="restart"
+        style={{
+          position: "absolute",
+          right: "0px",
+          top: "0px",
+          backgroundColor: "white",
+          marginRight: "5px",
+          marginTop: "5px",
+        }}
+        onClick={resetSim}
+      >
+        <RestartAltIcon />
+      </IconButton>
+    </Box>
+  );
 }
