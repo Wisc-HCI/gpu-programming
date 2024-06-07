4bd224ea19ff63d351ea4fb4a33b0dcd7558174a
stawberryslayer <ywang2776@wisc.edu>
2024-06-07 11:30:24 -0500

    Fix Blockly Interface reload from f86eedf

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,81 @@
-import React from 'react';
-import * as Blockly from 'blockly';
-import './App.css'; // Assuming index.css is renamed to App.css
+import React from "react";
+import * as Blockly from "blockly";
+import "./App.css"; // Assuming index.css is renamed to App.css
 
 // Import custom blocks and generators
-import { blocks } from './blocks/text';
-import { forBlock } from './generators/javascript';
-import { javascriptGenerator } from 'blockly/javascript';
-import './index.css';
-import TopBar from './components/Topbar';
-import BlocklyInterface from './components/Blockly-interface';
+import { blocks } from "./blocks/text";
+import { forBlock } from "./generators/javascript";
+import { javascriptGenerator } from "blockly/javascript";
+import "./index.css";
+import TopBar from "./components/Topbar";
+import BlocklyInterface from "./components/Blockly-interface";
 import urdf from "./Misty-Robot/misty.xacro?raw";
-import TrackerScreen from './tracker_components/TrackerScreen.jsx';
+import TrackerScreen from "./tracker_components/TrackerScreen.jsx";
 
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 
-import {
-  ReflexContainer,
-  ReflexSplitter,
-  ReflexElement
-} from 'react-reflex';
+import { ReflexContainer, ReflexSplitter, ReflexElement } from "react-reflex";
 
-import 'react-reflex/styles.css';
-import { Backdrop, Box, CircularProgress } from '@mui/material';
-import Simulator from './components/Simulator.jsx';
-import { SettingsModal } from './modals/Settings.jsx';
-import useStore from './Store.js';
-import { useShallow } from 'zustand/react/shallow';
-import GPTPanel from './components/GPTPanel.jsx';
-import { PromptModal } from './modals/PromptModal.jsx';
-import GPTConsole from './components/GPTConsole.jsx';
+import "react-reflex/styles.css";
+import { Backdrop, Box, CircularProgress } from "@mui/material";
+import Simulator from "./components/Simulator.jsx";
+import { SettingsModal } from "./modals/Settings.jsx";
+import useStore from "./Store.js";
+import { useShallow } from "zustand/react/shallow";
+import GPTPanel from "./components/GPTPanel.jsx";
+import { PromptModal } from "./modals/PromptModal.jsx";
+import GPTConsole from "./components/GPTConsole.jsx";
 
-const Item = styled('div')(({ theme }) => ({
-  textAlign: 'center',
-  height: "100%"
+const Item = styled("div")(({ theme }) => ({
+  textAlign: "center",
+  height: "100%",
 }));
 
 // Register the blocks and generator with Blockly
 Blockly.common.defineBlocks(blocks);
 Object.assign(javascriptGenerator.forBlock, forBlock);
 function App() {
-  const llmMode = useStore(useShallow(state => state.llmMode));
-  const isLLMProcessing = useStore(useShallow((state => state.llmProcessing)));
-  const headerHeight = useStore(useShallow((state => state.headerHeight)));
-  const fullScreenPanel = useStore(state => state.fullScreenPanel);
+  const llmMode = useStore(useShallow((state) => state.llmMode));
+  const isLLMProcessing = useStore(useShallow((state) => state.llmProcessing));
+  const headerHeight = useStore(useShallow((state) => state.headerHeight));
+  const fullScreenPanel = useStore((state) => state.fullScreenPanel);
 
   return (
     <Box width={"100vw"} height={`calc(100vh - ${headerHeight}px)`} padding={0}>
-      <TopBar/>
-      <ReflexContainer orientation='vertical' 
-          style={{
-            // backgroundColor: "red",
-            height: "100%",
-            width: "100vw",
-            // position: "fixed",
-          }}>
-        
+      <TopBar />
+      <ReflexContainer
+        orientation="vertical"
+        style={{
+          // backgroundColor: "red",
+          height: "100%",
+          width: "100vw",
+          // position: "fixed",
+        }}
+      >
         <ReflexElement flex={0.55}>
-          <Item style={{width: "100%", height:"100%"}}>
+          <Item style={{ width: "100%", height: "100%" }}>
             {fullScreenPanel ? <GPTPanel /> : null}
-            {<BlocklyInterface />}
+            {!fullScreenPanel && <BlocklyInterface />}
           </Item>
         </ReflexElement>
-        
+
         <ReflexSplitter propagate={true} />
-        
+
         <ReflexElement flex={0.45}>
-          <ReflexContainer orientation='horizontal'>
+          <ReflexContainer orientation="horizontal">
             <ReflexElement>
-              <Item>
-                {llmMode && <Simulator />}
-              </Item>
+              <Item>{llmMode && <Simulator />}</Item>
             </ReflexElement>
-            
+
             <ReflexSplitter propagate={true} />
-            
+
             <ReflexElement>
-              <Item>
-                {llmMode && <TrackerScreen/> }
-              </Item>
+              <Item>{llmMode && <TrackerScreen />}</Item>
             </ReflexElement>
-          
           </ReflexContainer>
         </ReflexElement>
       </ReflexContainer>
-      <Backdrop style={{color: "#fff", zIndex:1500}} open={isLLMProcessing}>
+      <Backdrop style={{ color: "#fff", zIndex: 1500 }} open={isLLMProcessing}>
         <CircularProgress color="inherit" />
       </Backdrop>
       <SettingsModal />
